4cc02ca3d9ba0d85ede801406b77beba5d808ff7
svxf <60079016+svxf@users.noreply.github.com>
2023-12-31 15:18:03 -0800

    Added Toggle Keybind
    -Added by MLegend (master_legend)

--- src/main/java/com/svxf/healthindicator/Main.java
+++ src/main/java/com/svxf/healthindicator/Main.java
@@ -1,10 +1,23 @@
 package com.svxf.healthindicator;
 
 import com.svxf.healthindicator.command.*;
+import com.svxf.healthindicator.utils.Utils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.EnumChatFormatting;
 import net.weavemc.loader.api.ModInitializer;
 import net.weavemc.loader.api.command.CommandBus;
+import net.weavemc.loader.api.event.*;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main implements ModInitializer {
+
+    public KeyBinding key = new KeyBinding("Health Indicators", 38, "Health Indicator Mod");
+
     @Override
     public void preInit() {
         System.out.println("Initializing HealthIndicator!");
@@ -17,5 +30,29 @@ public class Main implements ModInitializer {
         CommandBus.register(new Offset());
         CommandBus.register(new Scale());
         CommandBus.register(new Toggle());
+
+        EventBus.subscribe(this);
+        EventBus.subscribe(StartGameEvent.Post.class, e -> {
+            List<KeyBinding> keyBindings = new ArrayList<>(
+                    Arrays.asList(Minecraft.getMinecraft().gameSettings.keyBindings));
+            keyBindings.add(key);
+            Minecraft.getMinecraft().gameSettings.keyBindings = keyBindings.toArray(new KeyBinding[0]);
+        });
+    }
+
+    @SubscribeEvent
+    public void onKeyPress(KeyboardEvent e) {
+        if (Keyboard.getEventKey() == key.getKeyCode() && e.getKeyState()) {
+            Utils.print("Visiblity has set to "+ EnumChatFormatting.RED + !HealthIndicator.getInstance().getConfig().enabled + EnumChatFormatting.RESET + ".");
+            HealthIndicator.getInstance().getConfig().enabled = !HealthIndicator.getInstance().getConfig().enabled;
+        }
+    }
+
+    @SubscribeEvent
+    public void onMousePress(MouseEvent e) {
+        if (Mouse.getEventButton() == key.getKeyCode() + 100 && e.getButtonState()) {
+            Utils.print("Visiblity has set to "+ EnumChatFormatting.RED + !HealthIndicator.getInstance().getConfig().enabled + EnumChatFormatting.RESET + ".");
+            HealthIndicator.getInstance().getConfig().enabled = !HealthIndicator.getInstance().getConfig().enabled;
+        }
     }
 }
